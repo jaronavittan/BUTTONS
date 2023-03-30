97699ce168f4f196053508767ae661a0ae0b3461
jaronavittan <96601217+jaronavittan@users.noreply.github.com>
2023-03-30 12:04:31 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<DOCTYPE! html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+      <style>
+      button {
+        display: inline-block;
+        background-color: #7FFFD4;
+        padding: 20px;
+        width: 200px;
+        color: #DC143C;
+        text-align: center;
+      }
+      </style>
+    
+    <form method="GET">
+    <input type="hidden" name="name" value="value">
+    <button type="submit">OPEN NEW FOLDER</button>
+    </form>  
+    
+    <form method="GET">
+    <input type="hidden" name="name" value="value">
+    <button type="submit">ACCESS MY ACCOUNT</button>
+    </form>  
+    
+    <form method="GET">
+    <input type="hidden" name="name" value="value">
+    <button type="submit">REPLY TO OTHERS</button>
+    </form>  
+      
+    
+  </body>
+</html>
